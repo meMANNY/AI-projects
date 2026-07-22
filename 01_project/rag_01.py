b3b9b8ff119ac72eae8d64d585989c6ab0398fed
Aman Tripathi <amanttripathi02@gmail.com>
2026-07-22 20:14:15 +0530

    completed the first rag with indexing and retrieval

--- 01_project/rag_01.py
+++ 01_project/rag_01.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
+from langchain_openai import OpenAI, OpenAIEmbeddings
 from langchain_chroma import Chroma
-
+from openai import OpenAI
 load_dotenv()
-
+client = OpenAI(api_key=os.getenv("OPENAI_API"))
 pdf_path = Path(__file__).parent /"Aman_HR_Round.pdf"
 
 loader = PyPDFLoader(
@@ -17,8 +17,8 @@ loader = PyPDFLoader(
 
 docs = loader.load()
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=100,
-    chunk_overlap=40,
+    chunk_size=800,
+    chunk_overlap=200,
 
 )
 split_docs = text_splitter.split_documents(docs)
@@ -33,8 +33,38 @@ vectorstore = Chroma.from_documents(
     embedding=embeddings,
     collection_name="aman_hr_round",
 )
-print(vectorstore._collection.count())
-print(vectorstore._collection.name)
+
+user_query = input("Enter your query: ")
+
+results = vectorstore.similarity_search(
+    query=user_query,
+    k=3
+)
+context = "\n\n".join(doc.page_content for doc in results)
+
+
+SYSTEM_PROMPT = f"""You are a helpful assistant that answers questions based on the context provided.
+If the answer is not contained within the context, you should respond with "I don't know."
+Context: {context}
+"""
+
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": SYSTEM_PROMPT},
+        {"role": "user", "content": user_query}
+    ]
+)
+print(f"Assistant: {response.choices[0].message.content}")
+
+
+
+
+
+
+
+
+
 
 
 
