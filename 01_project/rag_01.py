e7258a05fcce076798c5fe600bed82b356fb2367
Aman Tripathi <amanttripathi02@gmail.com>
2026-07-22 19:29:41 +0530

    added vector embedding model and now indexing via chromadb

--- 01_project/rag_01.py
+++ 01_project/rag_01.py
@@ -1,5 +1,12 @@
+import os
+from dotenv import load_dotenv
+
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+load_dotenv()
+
 pdf_path = Path(__file__).parent /"Aman_HR_Round.pdf"
 
 loader = PyPDFLoader(
@@ -7,4 +14,17 @@ loader = PyPDFLoader(
 )
 
 docs = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=100,
+    chunk_overlap=40,
+
+)
+split_docs = text_splitter.split_documents(docs)
+
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-3-small",
+    
+)
+
+
 
