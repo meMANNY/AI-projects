e52a0331d54dbafd19cf01e0bfe5296457cb8efc
Aman Tripathi <amanttripathi02@gmail.com>
2026-07-22 19:41:40 +0530

    successfully passed indexing phase

--- 01_project/rag_01.py
+++ 01_project/rag_01.py
@@ -5,6 +5,8 @@ from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+
 load_dotenv()
 
 pdf_path = Path(__file__).parent /"Aman_HR_Round.pdf"
@@ -23,8 +25,16 @@ split_docs = text_splitter.split_documents(docs)
 
 embeddings = OpenAIEmbeddings(
     model="text-embedding-3-small",
-    
+    openai_api_key=os.getenv("OPENAI_API")
 )
+
+vectorstore = Chroma.from_documents(
+    documents = split_docs,
+    embedding=embeddings,
+    collection_name="aman_hr_round",
+)
+print(vectorstore._collection.count())
+print(vectorstore._collection.name)
 
 
 
